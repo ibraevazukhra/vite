8cb04ed6d71f21d6f78ded7e2a4959e6fa04802e
Ибраева Зухра Ильсуровна <300704izi@gmail.com>
2024-02-12 16:18:41 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>ПРИВЕТ!</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Кол-во нажатий {count}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Нажми на кнопку, и кол-во нажатий увеличится:)
+      </p>
+    </>
+  )
+}
+
+export default App
