b30d24db14ee7ed3d528392cfb8c8a777cbf92ac
Ибраева Зухра Ильсуровна <300704izi@gmail.com>
2024-02-21 10:23:00 +0300

    10

--- src/data.js
+++ src/data.js
@@ -2,21 +2,25 @@ export const catalog = [
     {
         id:1,
         name:'Рубашка 1',
-        price:'500р'
+        price:'500р',
+        count:0
     },
     {
         id:2,
         name:'Рубашка 2',
-        price:'600р'
+        price:'600р',
+        count:1
     },
     {
         id:3,
         name:'Рубашка 3',
-        price:'1000р'
+        price:'1000р',
+        count:0
     },
     {
         id:4,
         name:'Рубашка 4',
-        price:'70р'
+        price:'70р',
+        count:2
     },
 ]
